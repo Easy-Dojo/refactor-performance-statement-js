4afc3070ac483a8ffca09ffb9584ff0ad7cce200
Lingrui <392425595@qq.com>
2021-06-15 18:33:36 +0800

    init commit

---
+++ __tests__/printDramaBillingDetails.js
@@ -0,0 +1,14 @@
+import { statement } from '../src/printDramaBillingDetails'
+
+import invoices from '../data/invoices'
+import plays from '../data/plays'
+
+test('test statement', () => {
+  expect(statement(invoices[0], plays)).toBe(`Statement for BigCo
+  Hamlet: $650.00 (55 seats)
+  As You Like It: $580.00 (35 seats)
+  Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`);
+});
